dd1cee044cffe1a5905ca7a43fe17543396b951e
Karina Gorkava <infernormal@gmail.com>
2021-11-09 10:45:50 -0800

    Revert "Video CRUD operations ready for merge"

--- app/models/video.py
+++ app/models/video.py
@@ -1,16 +1,4 @@
 from app import db
-#from app import current_app
 
 class Video(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String)
-    release_date = db.Column(db.DateTime)
-    total_inventory = db.Column(db.Integer)
-
-    def to_dict(self):
-        return  {
-            "id": self.id,
-            "title": self.title,
-            "release_date": self.release_date,
-            "total_inventory": self.total_inventory
-        }
--- app/routes.py
+++ app/routes.py
@@ -1,89 +1,9 @@
-
-from flask import Blueprint, jsonify, request, make_response
-from flask.globals import session
 from app import db
-from app.models.video import Video
 from app.models.customer import Customer
+from flask import Blueprint, jsonify, request
 from datetime import datetime
+import os
 import requests
-from dotenv import load_dotenv
-import os 
-
-load_dotenv()
-
-videos_bp = Blueprint("videos", __name__, url_prefix="/videos")
-customer_bp = Blueprint("customer_bp", __name__,url_prefix="/customers")
-
-#VIDEO ROUTES
-
-@videos_bp.route("", methods=["GET", "POST"], strict_slashes=False)
-def create_videos():
-    if request.method == "POST":
-        request_body = request.get_json()
-        input_check = validate_video_input(request_body)
-        if input_check['error']: 
-            return make_response({"details":input_check['details']}), input_check['status_code']
-        else:    
-            new_video = Video(title=request_body['title'], release_date=request_body['release_date'], total_inventory=request_body['total_inventory'])
-            db.session.add(new_video)
-            db.session.commit()
-            return make_response(new_video.to_dict()), 201
-    elif request.method == "GET":
-        video_list = Video.query.all()
-        response = []
-        for my_video in video_list:
-            response.append(my_video.to_dict())
-        
-        return jsonify(response), 200
-
-@videos_bp.route("/<video_id>", methods=["GET", "PUT", "DELETE"], strict_slashes=False)
-def get_video(video_id):
-    try:
-        video_id = int(video_id)
-    except ValueError:
-        return "Error, please provide the integer id of the video", 400
-    my_video = Video.query.get(video_id)
-    if request.method == "GET":
-        if my_video is None:
-            return {"message": "Video " + str(video_id) + " was not found"}, 404
-        else:
-            return make_response(my_video.to_dict()), 200
-
-    elif request.method == "PUT":
-        request_body = request.get_json()
-        if my_video is None:
-            return {"message": "Video " + str(video_id) + " was not found"}, 404
-        else:
-            input_check = validate_video_input(request_body)
-            if input_check['error']: 
-                return make_response({"details":input_check['details']}), input_check['status_code']
-            else:  
-                my_video.title = request_body["title"]
-                my_video.release_date = request_body["release_date"]
-                my_video.total_inventory =request_body ["total_inventory"]
-                db.session.commit()        
-                return make_response(my_video.to_dict()), 200
-
-
-    elif request.method == "DELETE":
-        if my_video is None:
-            return {"message": "Video 1 was not found"}, 404
-        else:        
-            db.session.delete(my_video)
-            db.session.commit()
-            return make_response({"id":video_id}), 200
-
-
-def validate_video_input(request_body):
-    if "title" not in request_body:
-        return {"error":True, "details": "Request body must include title.", "status_code": 400}
-    elif "release_date" not in request_body:
-        return {"error":True, "details": "Request body must include release_date.", "status_code": 400}
-    elif "total_inventory" not in request_body:
-        return {"error":True, "details": "Request body must include total_inventory.", "status_code": 400}
-    else:
-        return {"error":False, "details": "", "status_code": 200}
-      
 
 customer_bp = Blueprint("customer_bp", __name__,url_prefix="/customers")
 
